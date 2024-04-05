75337c546ba298ef9988710cc2731b1d09fb2f68
Sean Gregory <chiefsgreg2622@gmail.com>
2024-04-05 18:17:35 -0400

    Updated the code to separate

--- COMP353ProjectTableGeneration/Program.cs
+++ COMP353ProjectTableGeneration/Program.cs
@@ -22,65 +22,73 @@ namespace COMP353ProjectTableGeneration
             foreach (Residence residence in residences)
                 builder.Append(residence.ToString() + ",\n");
             Polish(ref builder);
-            
+            Export(ref builder, "Residence");
+
             Person[] people = Person.MakePeople(100, residences);
             people = people.Distinct().ToArray();
             builder.Append("INSERT INTO Person VALUES\n");
             foreach (Person person in people)
                 builder.Append(person.ToString() + ",\n");
             Polish(ref builder);
-            
+            Export(ref builder, "Person");
+
             Employee[] employees = Employee.MakeEmployees(30, people);
             employees = employees.Distinct().ToArray();
             builder.Append("INSERT INTO Employee VALUES\n");
             foreach (Employee employee in employees)
                 builder.Append(employee.ToString() + ",\n");
             Polish(ref builder);
-            
+            Export(ref builder, "Employee");
+
             Facility[] facilities = Facility.MakeFacilities(25, employees);
             facilities = facilities.Distinct().ToArray();
             builder.Append("INSERT INTO Facility VALUES\n");
             foreach (Facility facility in facilities)
                 builder.Append(facility.ToString() + ",\n");
             Polish(ref builder);
-            
+            Export(ref builder, "Facility");
+
             WorksAt[] works = WorksAt.MakeWorksAt(45, employees, facilities);
             works = works.Distinct().ToArray();
             builder.Append("INSERT INTO Works_at VALUES\n");
             foreach (WorksAt work in works)
                 builder.Append(work.ToString() + ",\n");
             Polish(ref builder);
-            
+            Export(ref builder, "Works_at");
+
             LivesIn[] livesIn = LivesIn.MakeLivesIn(50, people, residences);
             livesIn = livesIn.Distinct().ToArray();
             builder.Append("INSERT INTO lives_in VALUES\n");
             foreach (LivesIn live in livesIn)
                 builder.Append(live.ToString() + ",\n");
             Polish(ref builder);
-            
+            Export(ref builder, "Lives_in");
+
             LivesWith[] livesWith = LivesWith.MakeLivesWith(50, people, employees);
             livesWith = livesWith.Distinct().ToArray();
             builder.Append("INSERT INTO lives_with VALUES\n");
             foreach (LivesWith live in livesWith)
                 builder.Append(live.ToString() + ",\n");
             Polish(ref builder);
-            
+            Export(ref builder, "Lives_with");
+
             Infection[] infections = Infection.MakeInfections(40, people);
             infections = infections.Distinct().ToArray();
             builder.Append("INSERT INTO Infection VALUES\n");
             foreach (Infection infection in infections)
                 builder.Append(infection.ToString() + ",\n");
             Polish(ref builder);
-            
+            Export(ref builder, "Infection");
+
             Vaccine[] vaccines = Vaccine.MakeVaccines(80, people, facilities);
             vaccines = vaccines.Distinct().ToArray();
             builder.Append("INSERT INTO Vaccine VALUES\n");
             foreach (Vaccine vaccine in vaccines)
                 builder.Append(vaccine.ToString() + ",\n");
             Polish(ref builder);
-            Export(builder);
+            Export(ref builder, "Vaccine");
         }
-        public static void Export(StringBuilder builder)
+        public static void Export(ref StringBuilder builder, string name)
         {
             string appPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
             int tableLocation = appPath.IndexOf("COMP353ProjectTableGeneration");
@@ -88,8 +96,9 @@ namespace COMP353ProjectTableGeneration
             appPath += "Inserts";
 
             // Write the string array to a new file named "WriteLines.txt".
-            using StreamWriter outputFile = new StreamWriter(Path.Combine(appPath, "Inserts.txt"));
+            using StreamWriter outputFile = new StreamWriter(Path.Combine(appPath, name + ".txt"));
             outputFile.WriteLine(builder);
+            builder.Clear();
         }
     }
 }
--- COMP353ProjectTableGeneration/README.md
+++
@@ -1,6 +0,0 @@
-Table Generation by Sean Gregory
-
-If you want to run the program, click on "COMP353ProjectTableGeneration.csproj" and run it
-
-
-Copyright Sean Gregory
