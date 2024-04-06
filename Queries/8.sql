848b15ad03fcedbeccf185c79f47076449987aee
Sean Gregory <57275515+seangregoryv8@users.noreply.github.com>
2024-04-06 16:27:34 -0400

    Made Query 8

---
+++ Queries/8.sql
@@ -0,0 +1,33 @@
+-- Query 8
+SELECT
+	f.Name AS Name,
+    f.Address AS Address,
+    f.City AS City,
+    f.Province AS Province,
+    f.Postal_Code AS 'Postal-Code',
+    f.Phone AS 'Phone Number',
+    f.Web_address AS 'Web Address',
+    f.Type AS Type,
+    f.Capacity AS Capacity,
+    e.Role AS 'General Manager',
+    COUNT(w.Employee) AS Num_Employees,
+    SUM(CASE WHEN e.Role = 'Doctor' THEN 1 ELSE 0 END) AS Num_Doctors,
+    SUM(CASE WHEN e.Role = 'Nurse' THEN 1 ELSE 0 END) AS Num_Nurses
+FROM Facility f
+LEFT JOIN Employee e ON f.Managed_by = e.SIN
+LEFT JOIN Works_at w ON f.Name = w.Facility
+GROUP BY
+	f.Name,
+    f.Address,
+    f.City,
+    f.Province,
+    f.Postal_code,
+    f.Phone,
+    f.Web_address,
+    f.Type,
+    f.Capacity,
+    e.Role
+ORDER BY
+	f.Province ASC,
+    f.City ASC,
+    f.Type ASC;
