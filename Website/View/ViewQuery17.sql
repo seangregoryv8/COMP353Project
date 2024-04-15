258b84c044f951c0b5bfcd9153886be1bcaf7e75
rogersi9 <69977233+rogersi9@users.noreply.github.com>
2024-04-15 16:41:46 -0400

    Fixed query 16, 17

--- Website/View/ViewQuery17.sql
+++ Website/View/ViewQuery17.sql
@@ -1,17 +1,17 @@
 Select Info.name, Info.Role, Info.NumEmployee, IfNull(Info.NumEmployee, 0) - IfNull(InfectionInfo.NumInfections,0) As NeverInfected
 From
-	(Select Facility.name, Employee.Role, Count(DISTINCT Works_At.Employee) As NumEmployee
-	From Facility, Works_At, Employee
-	Where Facility.Name = Works_At.Facility
-	AND Works_At.Employee = Employee.SIN
+	(Select Facility.name, Employee.Role, Count(DISTINCT Works_at.Employee) As NumEmployee
+	From Facility, Works_at, Employee
+	Where Facility.Name = Works_at.Facility
+	AND Works_at.Employee = Employee.SIN
     Group By Facility.Name, Employee.Role) as Info
     Left Join
     (Select Facility.Name, Employee.Role, Count(Distinct Infection.Person) as NumInfections
-    From Infection, Works_At, Facility, Employee
+    From Infection, Works_at, Facility, Employee
     Where
-    Facility.Name = Works_At.Facility
-	AND Works_At.Employee = Infection.Person
-    AND Works_At.Employee = Employee.SIN
+    Facility.Name = Works_at.Facility
+	AND Works_at.Employee = Infection.Person
+    AND Works_at.Employee = Employee.SIN
     Group By Facility.Name, Employee.Role) As InfectionInfo
     On InfectionInfo.Name = Info.Name AND Info.Role = InfectionInfo.Role
 Group By Info.Name, Info.Role
