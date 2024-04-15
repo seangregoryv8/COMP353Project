511f1276084af9fbe659e515f8667c54efd191d6
rogersi9 <69977233+rogersi9@users.noreply.github.com>
2024-04-15 16:27:51 -0400

    Fixed query 16

--- Website/View/ViewQuery16.sql
+++ Website/View/ViewQuery16.sql
@@ -1,17 +1,17 @@
 Select Info.name, Info.Role, Info.NumEmployee, IfNull(InfectionInfo.NumInfections, 0) As NumInfections
 From
-	(Select Facility.name, Employee.Role, Count(Distinct Works_At.Employee) As NumEmployee
-	From Facility, Works_At, Employee
-	Where Facility.Name = Works_At.Facility
-	AND Works_At.Employee = Employee.SIN
+	(Select Facility.name, Employee.Role, Count(Distinct Works_at.Employee) As NumEmployee
+	From Facility, Works_at, Employee
+	Where Facility.Name = Works_at.Facility
+	AND Works_at.Employee = Employee.SIN
     Group By Facility.Name, Employee.Role) as Info
     Left Join
     (Select Facility.Name, Employee.Role, COUNT(Infection.Person) as NumInfections
-    From Infection, Works_At, Facility, Employee
+    From Infection, Works_at, Facility, Employee
     Where
-    Facility.Name = Works_At.Facility
-	AND Works_At.Employee = Infection.Person
-    AND Works_At.Employee = Employee.SIN
+    Facility.Name = Works_at.Facility
+	AND Works_at.Employee = Infection.Person
+    AND Works_at.Employee = Employee.SIN
 	AND Infection.Date > DATE_SUB(CURDATE(), INTERVAL Infection.Quarantine_Period DAY)
 	AND Infection.Date <= CURDATE()
     Group By Facility.Name, Employee.Role) As InfectionInfo
