8a0bb78912fce6e0477a33a33c33d26eb7693cac
Marcus Sharma <marcusmmm007@hotmail.com>
2024-04-15 21:34:31 -0400

    e

--- Website/View/ViewQuery11.sql
+++ Website/View/ViewQuery11.sql
@@ -1,7 +1,7 @@
 -- Query 11
 SELECT Residence.Address, Residence.Type, Person.FirstName, Person.LastName, Person.Occupation, Lives_with.Relationship  
 FROM Lives_in, Lives_with, Person, Employee, Residence
-Where Employee.SIN = '4826635732'
+Where Employee.SIN = ?
 AND Lives_in.Person = Employee.SIN
 AND Lives_with.Employee = Employee.SIN 
 AND Person.SIN = Lives_with.Person
